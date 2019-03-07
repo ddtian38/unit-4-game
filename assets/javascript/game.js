16578af0fa6d1da64dc148ec88f08b5aefe89635
Daniel Tian <danieltian31@yahoo.com>
2019-03-06 18:18:58 -0600

    finish with the JS file to run the game. need to add increment in attack points for hero after the hero has attacked/defeated an enemy.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+
+
 var characters = { "Captain Rex": {attack: 20,
                         hitPoints: 100
                 },
@@ -13,59 +15,82 @@ var characters = { "Captain Rex": {attack: 20,
                     hitPoints: 140
                     
                 }
-
-
 }
+var heroDead = false;
+var villianDead = false;
+var thereIsVillian = true;
+var charcList = $('.friendly');
+
+$('#rex .hit-points').text(characters["Captain Rex"].hitPoints);
+$('#cody .hit-points').text(characters["Commander Cody"].hitPoints);
+$('#krell .hit-points').text(characters["General Krell"].hitPoints);
+$('#grevious .hit-points').text(characters["General Grevious"].hitPoints);
+
+function choosingHeroAndEnemies(){
+    $(this).addClass("hero");
+    choosingEnemies();
+}
+
+function choosingEnemies(){
+    for (var i = 0; i < charcList.length; i++){
+        if(!(charcList.eq(i).hasClass("hero"))){
+            var enemy = charcList.eq(i).removeClass("friendly").addClass("hostile").detach();
+            enemy.appendTo($('.enemies'));
+        }
+    }
+}
+function chooseVillian () {
+    $(this).addClass("villian");
+    var defender =  $(this).detach();
+    defender.appendTo($('.defender'));
+ };
+
+ function runGame(){
+    thereIsVillian = ($(".villian").length > 0);
+    var hero = characters[$(".hero .name").text()];
+    var villian =  characters[$(".villian .name").text()];
+    
+    if(thereIsVillian){
+      var heroAttack = hero["attack"];
+      var villianAttack = villian["attack"];
+      console.log(villianAttack);
+      $("#attack-status").text("You attacked " + $(".villian .name").text() + " for " + heroAttack +  " damage. "  +  $(".villian .name").text()+ " attacked you back" + " for " + villianAttack + " damage.");
+      hero.hitPoints -= villianAttack;
+      villian.hitPoints -= heroAttack;
+      $('.hero .hit-points').text(hero.hitPoints);
+      $('.villian .hit-points').text(villian.hitPoints);
+      villianDead = (villian.hitPoints <= 0);
+      heroDead = (hero.hitPoints <= 0);
+  
+      if(heroDead){
+          $("#attack-status").text("Game over. Reset to play.")
+          $("#attack-status").append($("<button class=\"reset\"> Reset </button>"));
+        $(document).off("click", ".friendly", choosingHeroAndEnemies);
+        $(document).off("click", ".hostile", chooseVillian);
+        $(document).off("click", "#attack", runGame);
+      }
+        if(villianDead){
+            $(".defender").empty();
+            $("#attack-status").text("Congratlations! " + villian + " has been defeated. Select another enemy fight.")
+        }
+      }
+      else{
+          $("#attack-status").text("No enemy is here. Please select an enemy.");
+      }
+  }
 
 
 $(document).ready(function(){
 
-    $('#rex .hit-points').text(characters["Captain Rex"].hitPoints);
-    $('#cody .hit-points').text(characters["Commander Cody"].hitPoints);
-    $('#krell .hit-points').text(characters["General Krell"].hitPoints);
-    $('#grevious .hit-points').text(characters["General Grevious"].hitPoints);
+    $(document).on("click", ".friendly", choosingHeroAndEnemies);
 
-    console.log("Running program");
+    $(document).on("click", ".hostile", chooseVillian);
 
 
-$(".hostile").on("click", function(){
-        console.log("adding hostile")
-        $(this).addClass("hostile-selected");
-       var enemy =  $(this).detach();
-       enemy.appendTo($('.defender'));
-    
-    });
+$(document).on("click", "#attack", runGame);
 
-
-
-$(".friendly").on("click", function(){
-    console.log("x");
-    var charcList = $('.friendly');
-    console.log(charcList);
-    $(this).addClass("friendly-selected");
-    for (var i = 0; i < charcList.length; i++){
-        console.log("xxx");
-        if(!(charcList.eq(i).hasClass("friendly-selected"))){
-            charcList.eq(i).removeClass("friendly").addClass("hostile");
-            var detach = charcList.eq(i).detach();
-            detach.appendTo($('.enemies'));
-        }
-    }
-});
-
-
-
-
-
-
-$("button").on("click", function(){
-
-    var p1 = $("<p>").text("You attacked " + $(".hostile-selected .name").text() + " for " + characters[$(".friendly-selected .name").text()].attack +  "damage.");
-    var p2 = $("<p>").text($(".hostile-selected .name").text()+ " attacked you back" +  + " for " + characters[$(".hostile-selected .name").text()].attack + "damage.");
-    p1.appendTo($(".attack-status"));
-    p2.appendTo($(".attack-status"));
-    var attackStatusExist = 
-
+$(document).on("click", ".reset", function(){
+    location.reload();
 })
 
 
